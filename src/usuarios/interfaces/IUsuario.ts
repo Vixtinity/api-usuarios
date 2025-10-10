97db5d6116f344a4e96825ba292067ef66a8984f
Vixtinity <08062006ismael@gmail.com>
2025-10-10 08:13:38 +0200

    cambios

--- src/usuarios/interfaces/IUsuario.ts
+++ src/usuarios/interfaces/IUsuario.ts
@@ -2,11 +2,22 @@
 //interfaces --> a las estructuras de datos que se transmiten por la red
 //tipos --> se sueles aplicar para defini estructuras de datos internas o esquemas de datos
 //dtos --> para valida estructuras de datos que se reciben por la red
+export interface IAddress{
+    calle:string;
+    numero:string;
+    ciudad:string;
+    pais:string;
+}
 export interface iUser{
     id:number;
     name:string;
     emal:string;
     edad:number;
+    rol: string;
+    telefonos?:string[];
+    nif?:string;
+    esdelMadrid?:boolean;
+    direcciones:IAddress[];
 }
 
 type tUsuario = {
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb';
+import { iUser } from './interfaces/IUsuario';
 
+type Data = {
+    users: iUser[]
+}
 @Injectable()
 export class UsuariosService {
     private db: Low<any>;
@@ -13,5 +17,22 @@ export class UsuariosService {
     async findAll(){
         await this.db.read(); //se bloquea
         return this.db.data.users;
+    }
+    
+
+    async findOne(id:string){
+        const data = await this.db.read();
+        this.db.data.users.find(usuario => usuario.id === id);
+    }
+
+    async new(usuario: iUser){
+        await this.db.read();
+        this.db.data.users.push(usuario);
+        this.db.write();
+        console.log(this.db.data.users);
+        return {
+            msg: 'Usuario creado correctamente',
+            data: this.db.data.uses
+        }
     }
 }
