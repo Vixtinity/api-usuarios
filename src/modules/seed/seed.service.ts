6de3bd0e2e1ea67f495454a56b854be85d2fd54b
Vixtinity <08062006ismael@gmail.com>
2025-10-31 12:08:49 +0100

    cambios

--- src/modules/seed/seed.service.ts
+++ src/modules/seed/seed.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSeedDto } from './dto/create-seed.dto';
 import { UpdateSeedDto } from './dto/update-seed.dto';
+import { ClienteService } from '../clientes/cliente.service';
 
 @Injectable()
 export class SeedService {
+
+  constructor(private readonly ClientesService: ClienteService) {}
   loadData() {
-    return { message: 'Data loaded successfully' };
+    console.log('Loading seed data...');
+    this.insertNewClientes();
+    return {
+      message: 'Data loaded successfully',
+      data: seedCLientes
+    };
+  }
+  private insertNewClientes() {
+    const insertPromisesClientes = [];
+    seedClientes.forEach((cliente: any) =>  {
+      insertPromisesClientes.push(this.ClientesService.new(cliente));
+      console.log(cliente.apellidos);
+    });
   }
 }
